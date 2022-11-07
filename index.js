f466c8364bba4370e310da54a8d51096338b92bc
Allison Ause <allison.ause@gmail.com>
2022-11-07 15:16:26 -0800

    reverted back to Gift schema for demo

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 // import('dotenv').config()
 import mongoose from 'mongoose'
-import User from './models/Gift.js'
+import Gift from './models/Gift.js'
 
 // Connect to MongoDB Atlas Cluster
-
 mongoose.connect(
   `mongodb+srv://tester:password123456@aause.bos7dtf.mongodb.net/?retryWrites=true&w=majority`,
 ) // why does this not work as a password, leading to a "handshake error"?
 
 // CREATE a new Gift object & insert it into database
-const gift = await User.create({
+const gift = await Gift.create({
   idea: 'Sword Umbrella',
   link: 'https://www.swordsdirect.com/swords-rapier-umbrella.html',
   price: 70,
 })
 
-// Find a single Gift
+// FIND a single Gift
 const firstGift = await Gift.findOne({})
 console.log('firstGift', firstGift)
 
-// Update a single Gift locally, which is then saved to the database
+// UPDATE a single Gift locally, which is then saved to the database
 gift.price = 55
 await gift.save()
 console.log('updatedGift', gift)
@@ -35,8 +34,10 @@ const specificGift = await Gift.findById(
 console.log('specificGift', specificGift)
 
 //DELETE BY ID
-const deletedGift = Gift.findByIdAndDelete('6365543b236f18646aad5012')
-const confirmation = Gift.exists({ _id: '6365543b236f18646aad5012' })
+const deletedGift = await Gift.findByIdAndDelete('6365543b236f18646aad5012')
+// maybe await or maybe need to .exec()
+
+const confirmation = await Gift.exists({ _id: '6365543b236f18646aad5012' })
 // does NOT return the deletedGift
 console.log('deletedGift', deletedGift)
 console.log('confirmation', confirmation)
